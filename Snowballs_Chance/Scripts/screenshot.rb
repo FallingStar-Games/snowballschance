cda739c1105f1818d91d5e6094b436f5f2fe005d
shinyjiggly <agenttat@gmail.com>
2026-08-25 23:10:57 -0700

    quickie buggie fixie
    - i did the things
    
    to-do:
    - many more things

--- Snowballs_Chance/Scripts/screenshot.rb
+++ Snowballs_Chance/Scripts/screenshot.rb
@@ -34,59 +34,10 @@
     alias new_snop update
   end
   def self.update
-    if Input.trigger?(Input::F5)
+    if Input.trigger?(Input::F2)
       #Keys.trigger?(Screen::SnapShot_Key) 
       shot #hahaha, GET IT?
     end
     new_snop
   end
 end
-
-=begin
-module Screen
-  SnapShot_Key = $keyboard["snap"] #Input::A # Q key
-  @screen = Win32API.new 'screenshot', 'Screenshot', %w(l l l l p l l), ''
-  @readini = Win32API.new 'kernel32', 'GetPrivateProfileStringA', %w(p p p p l p), 'l'
-  @findwindow = Win32API.new 'user32', 'FindWindowA', %w(p p), 'l'
-  module_function
-
-  def shot(file = 'Screenshot_', typ = 2)
-    
-    Audio.se_play('Audio/SE/camera', 100, 100)
-    
-    typname = typ == 0 ? '.bmp' : typ == 1 ? '.jpg' : '.png'
-    @file_index = 0
-    @file_index += 1 while FileTest.exist?('Snapshots/' + file + @file_index.to_s + typname)
-    file_name = 'Snapshots/' + file + @file_index.to_s + typname
-    @screen.call(0,0,640,480,file_name,handel,typ)
-    
-        #show special message
-    unless $scene.is_a?(Scene_Title) or $scene.is_a?(Scene_Splash)
-      $game_temp.common_event_id = 45
-    end
-#get this bitch working
-
-  end
-
-  def handel
-    game_name = "\0" * 256
-    @readini.call('Game', 'Title', '', game_name, 255, '.\Game.ini')
-    game_name.delete!('\0')
-    
-    return @findwindow.call('RGSS Player',game_name)
-  end
-end
-
-module Input
-  class << self
-    alias new_snop update
-  end
-  def self.update
-    if Keys.trigger?(Screen::SnapShot_Key) 
-      #Input.trigger, Keyboard.check
-      Screen.shot #hahaha, GET IT?
-    end
-    new_snop
-  end
-end
-=end
